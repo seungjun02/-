e5f7698c90e3025c645348347317795e2d7a672e
seungjun02 <100888387+seungjun02@users.noreply.github.com>
2024-01-05 22:44:41 +0900

    Update g2.py

--- Greedy/g2.py
+++ Greedy/g2.py
@@ -1,1 +1,14 @@
+import time
 
+N, M, K = map(int, input().split())
+data = list(map(int, input().split()))
+
+start_time = time.time()
+data.sort()
+
+print((M // K) * data[N-1] *K + (M % K) * data[N-2])
+
+end_time = time.time()
+
+execution_time = end_time - start_time
+print(f"실행 시간: {execution_time} 초")
